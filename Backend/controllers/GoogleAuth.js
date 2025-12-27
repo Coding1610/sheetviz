7de9db02ec9202766a03dd4f6efec5c540c9970a
Coding1610 <yp.70010@gmail.com>
2025-12-27 12:00:19 +0530

    final commit

--- Backend/controllers/GoogleAuth.js
+++ Backend/controllers/GoogleAuth.js
@@ -37,8 +37,8 @@ exports.GoogleAuth = async(req,res,next) => {
         // create cookie
         res.cookie('cookie_name', token , {
             httpOnly:true,
-            secure:process.env.NODE_ENV === 'production',
-            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure:true,
+            sameSite:none,
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 Days
             path:'/'
         });
